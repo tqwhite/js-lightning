c13edfd1f0f1c68ac55130fc259b94abd3c42bf6
TQ White II <tq@justkidding.com>
2021-05-07 17:32:50 -0500

    Hello Worldgit add --all .

---
+++ startAll.js
@@ -0,0 +1,45 @@
+#!/usr/local/bin/node
+'use strict';
+// const qtoolsGen = require('qtools');
+// const qtools = new qtoolsGen(module, { updatePrototypes: true });
+
+//npm i qtools-functional-library
+//npm i qtools-config-file-processor
+//npm i qtools-parse-command-line
+
+// const configFileProcessor = require('qtools-config-file-processor');
+//const configSegmentName=require('path').basename(__filename).replace(/\.\w+$/, '');
+//const configPath=path.join(schUtilitiesProjectRoot, 'configs','instanceSpecific', PROJECT_NAMEConfigDirName, 'systemConfig.ini')
+//const config = configFileProcessor.getConfig(configPath)['MODULE_NAME']; 
+
+// const commandLineParser = require('qtools-parse-command-line');
+// const commandLineParameters = commandLineParser.getParameters();
+
+const qt = require('qtools-functional-library');
+//console.dir(qt.help());
+
+console.log(`HELLO FROM: ${__dirname}`);
+
+//START OF moduleFunction() ============================================================
+
+const moduleFunction = function(args={}) {
+
+	const workingFunctionActual=localArgs=>operatingArgs=>{
+	
+		return "hello";
+	
+	}
+	
+
+	const workingFunction=workingFunctionActual(Object.assign({}, args));
+
+	return ({workingFunction});
+};
+
+//END OF moduleFunction() ============================================================
+
+module.exports = args=>new moduleFunction(args)
+//module.exports = moduleFunction;
+//module.exports = new moduleFunction();
+//moduleFunction().workingFunction().qtDump();
+
